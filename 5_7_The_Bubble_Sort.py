017e494ae9c8060483d4023189e9a2f20b71b16e
huiyang lu <luchocoice@gmail.com>
2018-12-02 23:56:16 +0800

    Create 5_7_The_Bubble_Sort.py

---
+++ 5_7_The_Bubble_Sort.py
@@ -0,0 +1,26 @@
+def bubbleSort(alist):
+    for passnum in range(len(alist)-1,0,-1):
+        for i in range(passnum):
+            if alist[i]>alist[i+1]:
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+
+alist1 = [54,26,93,17,77,31,44,55,20]
+print(bubbleSort(alist1))
+
+def shortBubbleSort(alist):
+    exchanges = True
+    passnum = len(alist)-1
+    while passnum > 0 and exchanges:
+       exchanges = False
+       for i in range(passnum):
+           if alist[i]>alist[i+1]:
+               exchanges = True
+               temp = alist[i]
+               alist[i] = alist[i+1]
+               alist[i+1] = temp
+       passnum = passnum-1
+
+alist2=[20,30,40,90,50,60,70,80,100,110]
+print(shortBubbleSort(alist2))
