9bd8ab976df7588dc7b24a44ced1567e8b883443
huiyang lu <luchocoice@gmail.com>
2018-12-29 23:58:22 +0800

    Update 7_6_Implementation.py

--- 7_6_Implementation.py
+++ 7_6_Implementation.py
@@ -55,6 +55,7 @@ g = Graph()
 for i in range(6):
     g.addVertex(i)
 print(g.vertList)
+
 g.addEdge(0,1,5)
 g.addEdge(0,5,2)
 g.addEdge(1,2,4)
@@ -64,6 +65,7 @@ g.addEdge(3,5,3)
 g.addEdge(4,0,1)
 g.addEdge(5,4,8)
 g.addEdge(5,2,1)
+
 for v in g:
     for w in v.getConnections():
         print("( %s , %s )" % (v.getId(), w.getId()))
